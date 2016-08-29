310a6b60b0bad244dadacbd106f5a9ca00ec523e
yogeshgcpo <yogesh.gcpo@gmail.com>
2016-08-29 20:47:33 +0200

    First Version

---
+++ src/examples/Hello.java
@@ -0,0 +1,60 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Simple Hello servlet.
+ */
+
+public final class Hello extends HttpServlet {
+
+
+    /**
+     * Respond to a GET request for the content produced by
+     * this servlet.
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are producing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+      throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();        
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Sample Application Servlet Page</title>");
+        writer.println("</head>");
+        writer.println("<body bgcolor=white>");
+
+        writer.println("<table border=\"0\" cellpadding=\"10\">");
+        writer.println("<tr>");
+        writer.println("<td colspan=\"2\">");
+        writer.println("<img src=\"images/GCP_Logo.png\">");
+        writer.println("</td>");
+		writer.println("</tr>");
+		writer.println("<tr>");
+        writer.println("<td colspan=\"2\">");
+        writer.println("<h1>Sample Application Servlet</h1>");
+        writer.println("</td>");
+        writer.println("</tr>");
+        writer.println("</table>");
+
+        writer.println("This is the output of a servlet that is part of");
+        writer.println("the GCP application.");
+        writer.println("</br></br>");
+		writer.println("<a href=\"/gcpdemo\">Back</a>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+} 
---
+++ web/hello.jsp
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>Sample Application JSP Page</title>
+  </head>
+
+  <body bgcolor=white>
+
+  <table border="0" cellpadding="10">
+    <tr>
+      <td align=center>
+        <img src="images/GCP_Logo.png">
+      </td>
+	  <td align=center/>
+    </tr>
+	<tr>
+	<td colspan="2">
+         <h1>Sample Application JSP Page</h1>
+      </td>
+    </tr>
+	
+  </table>
+
+  <br />
+  <p>This is the output of a JSP page that is part of the GCP Demo application.</p>
+
+  <%= new String("Hello!") %>
+  <a href="/gcpdemo">Back</a>
+  </body>
+</html> 
